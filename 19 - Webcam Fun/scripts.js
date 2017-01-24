36b249c88c930be2d41ba2d2b5c207e8b4d384ea
Carlos Ferreira <carlosprovisorio@icloud.com>
2017-01-23 19:53:34 -0500

    define paint canvas func and set interval

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -12,8 +12,18 @@ function getVideo() {
     video.play();
     })
     .catch(err => {
-      console.log(`Don't do that`, err);
+      console.error(`Don't do that`, err);
     });
 }
 
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+  }, 16);
+}
 getVideo();
